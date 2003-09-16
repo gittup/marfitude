5fa022a86e33c054a09888073007f872829b6dfe
Marf <Marf>
2003-09-16 03:40:56 +0000

    FloatRand function

--- util/myrand.c
+++ util/myrand.c
@@ -5,3 +5,9 @@ int IntRand(int x)
 {
 	return (int)((double)x * rand()/(RAND_MAX+1.0));
 }
+
+// return a float from 0.0 to 1.0
+float FloatRand()
+{
+	return (float)rand() / (float)(RAND_MAX+1.0);
+}
