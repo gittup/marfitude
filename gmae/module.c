9591d9d5661d77df6234d5fe7a437f5e6bda16a0
marf <marf>
2004-06-28 03:37:11 +0000

    Added some Logs, make a check for the return of Player_GetModule

--- gmae/module.c
+++ gmae/module.c
@@ -9,6 +9,7 @@
 #include "log.h"
 
 #include "memtest.h"
+#include "fatalerror.h"
 #include "sdlfatalerror.h"
 
 Mix_Music *modMusic = NULL;
@@ -32,13 +33,20 @@ int StartModule(char *modFile)
 	}
 	Player_TogglePause();
 	Log(("Mod ready\n"));
+
 	mod = Player_GetModule();
+	if(mod == NULL) {
+		Error("retrieving module with MikMod's Player_GetModule()");
+		return 0;
+	}
+
 	mod->loop = 0; /* don't want to keep looping forever! */
 
 	if(CfgEq("sound.interpolation", "yes"))
 		md_mode |= DMODE_INTERP;
 	else
 		md_mode &= !DMODE_INTERP;
+	Log(("interp mode set\n"));
 	return 1;
 }
 
