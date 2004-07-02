c2acd32a4289f9fbba135c000f4d661c281c7051
marf <marf>
2004-07-02 05:16:10 +0000

    Added the enum check

--- gmae/chktextures.pl
+++ gmae/chktextures.pl
@@ -8,6 +8,6 @@ chomp(@img);
 for($x=0;$x<=$#img;$x++)
 {
 	$img[$x] =~ s/\.\/images\/(.*)\.png/$1/;
-	$inf = `grep "TEX_$img[$x]" *.c`;
+	$inf = `grep "TEX_$img[$x]" *.c; grep "T_$img[$x]" *.c`;
 	if(length($inf) == 0) {print "$img[$x] not used\n";}
 }
