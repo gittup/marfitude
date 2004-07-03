89d484e0be8dbf7175cd49fcd0ac357de0d44154
marf <marf>
2004-07-03 03:46:31 +0000

    Added debug config

--- util/Makefile.mk
+++ util/Makefile.mk
@@ -2,6 +2,10 @@ EXTRACFLAGS = `$(SDL_CONFIG) --cflags` $(BDECFLAGS)
 
 TARGET = libmarf
 
+ifeq ($(CONFIG_DEBUG_MEM),1)
+EXTRACFLAGS += -DDEBUG_MEM
+endif
+
 include $(MK)/lang/c.mk
 
 include $(MK)/tgt/staticlib.mk
