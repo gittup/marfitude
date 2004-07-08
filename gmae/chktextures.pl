b3cbb5b11064ddddbfa27f21042dc5e4e47b80a7
marf <marf>
2004-07-08 04:07:31 +0000

    Prints used textures now too

--- gmae/chktextures.pl
+++ gmae/chktextures.pl
@@ -10,4 +10,5 @@ for($x=0;$x<=$#img;$x++)
 	$img[$x] =~ s/\.\/images\/(.*)\.png/$1/;
 	$inf = `grep "TEX_$img[$x]" *.c; grep "T_$img[$x]" *.c`;
 	if(length($inf) == 0) {print "$img[$x] not used\n";}
+	else {print "$img[$x]\n";}
 }
