7e4b59bac8d619c2b17dd3ec69b1e674f25278bd
marf <marf>
2004-07-17 06:56:20 +0000

    install / uninstall targets

--- gmae/wam/Makefile.mk
+++ gmae/wam/Makefile.mk
@@ -1,3 +1,9 @@
+INSTALL_DIR = wam
+INSTALL_LIST = README
+include $(MK)/tgt/text.mk
+
+uninstall_files: $(wildcard $(INSTALL_ROOT)/$(INSTALL_DIR)/*.wam)
+
 project: $(wildcard $(d)/*.wam)
 $(d)/%.wam: $(src_gmae)/wam.c
 	@$(call RM,$@)
