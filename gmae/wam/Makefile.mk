9832d19ece636710467a23114a49034b947652c1
marf <marf>
2004-07-28 03:23:26 +0000

    Removed double-slash

--- gmae/wam/Makefile.mk
+++ gmae/wam/Makefile.mk
@@ -2,7 +2,7 @@ INSTALL_DIR = $(DATADIR)/wam
 INSTALL_LIST = README
 include $(MK)/tgt/text.mk
 
-UNINSTALL_FILES += $(wildcard $(DESTDIR)/$(INSTALL_DIR)/*.wam)
+UNINSTALL_FILES += $(wildcard $(subst //,/,$(DESTDIR)/$(INSTALL_DIR)/*.wam))
 
 project: $(wildcard $(d)/*.wam)
 $(d)/%.wam: $(src_gmae)/wam.c
