64687f5913613697c9eb02d560bb5c5d7fd7c210
marf <marf>
2004-08-05 04:25:13 +0000

    Removed $(MK) and src_ variables

--- gmae/wam/Makefile.mk
+++ gmae/wam/Makefile.mk
@@ -1,9 +1,9 @@
 INSTALL_DIR = $(DATADIR)/wam
 INSTALL_LIST = README
-include $(MK)/tgt/text.mk
+include mk/tgt/text.mk
 
 UNINSTALL_FILES += $(wildcard $(subst //,/,$(DESTDIR)/$(INSTALL_DIR)/*.wam))
 
 project: $(wildcard $(d)/*.wam)
-$(d)/%.wam: $(src_gmae)/wam.c
+$(d)/%.wam: gmae/wam.c
 	@$(call RM,$@)
