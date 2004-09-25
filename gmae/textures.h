420b3af69c0923b3d8957111fa0390b445feb202
marf <marf>
2004-09-25 15:14:53 +0000

    What was that crap doing in there still?

--- gmae/textures.h
+++ gmae/textures.h
@@ -1,4 +1,5 @@
 #include "SDL_opengl.h"
+
 GLuint LoadTexture(const char *filename);
 GLuint TextureNum(const char *name);
 int InitTextures(void);
@@ -8,23 +9,3 @@ struct tex_entry {
 	GLuint texture;
 	char *name;
 };
-
-enum texoffset {
-	T_BlueNova,
-	T_BlueStar,
-	T_Clovers,
-	T_ElectricBlue,
-	T_Fireball,
-	T_FlatlandFiery,
-	T_Laser,
-	T_Lava,
-	T_Parque,
-	T_Parque3,
-	T_Slate,
-	T_StarBurst,
-	T_StarCenter,
-	T_SunBurst,
-	T_SunCenter,
-	T_Target,
-	T_Title,
-	T_Walnut};
