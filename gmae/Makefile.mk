f3776603132ac4101befaf34e3ad34406080a534
marf <marf>
2004-11-22 03:10:29 +0000

    Use new EXEC variable, removed includes of Recurse.mk

--- gmae/Makefile.mk
+++ gmae/Makefile.mk
@@ -3,24 +3,4 @@ INCDIRS = gmae/sdl_mixer gmae/sdl_mixer/mikmod
 EXTRALDFLAGS = -lm
 LIBS = util/libmarf.a gmae/sdl_mixer/libsdl_mixer.a
 TARGET = marfitude
-include mk/tgt/program.mk
-
-subdir := wam
-include mk/Recurse.mk
-subdir := sdl_mixer
-include mk/Recurse.mk
-subdir := music
-include mk/Recurse.mk
-subdir := sounds
-include mk/Recurse.mk
-subdir := images
-include mk/Recurse.mk
-subdir := data
-include mk/Recurse.mk
-subdir := docs
-include mk/Recurse.mk
-
-ifeq ($(ARCH),mingw)
-subdir := dlls
-include mk/Recurse.mk
-endif
+EXEC = program
--- gmae/sdl_mixer/Makefile.mk
+++ gmae/sdl_mixer/Makefile.mk
@@ -2,7 +2,4 @@ PACKAGES = sdl
 EXTRACFLAGS += -DMOD_MUSIC
 INCDIRS = $(d)/mikmod
 LIBS = $(d)/mikmod/libsdl_mikmod.a
-include mk/tgt/staticlib.mk
-
-subdir := mikmod
-include mk/Recurse.mk
+EXEC = staticlib
