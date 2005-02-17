f323ce6bd48418ef5ea39bdfb67f7bf4735610a4
marf <marf>
2005-02-17 04:49:07 +0000

    Made the structure const so users can't modify the data easily

--- gmae/fft.h
+++ gmae/fft.h
@@ -17,14 +17,19 @@
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+/** This structure contains FFT data for the song that is played. It can be
+ * used after a call to init_fft()
+ */
 struct fft_data {
-	int *data;            /* len worth of data points */
-	unsigned int len;     /* length of above data, 0 if fft is not inited */
-	unsigned int log_len; /* log2(length) */
-	int cur_max;          /* The max data[i] for the current sample set */
-	int hist_max;         /* The max data[i] since last init_fft() */
-	int max;              /* The theoretical max, based on sample types */
+	const int *data;      /**< len worth of data points */
+	unsigned int len;     /**< length of above data, 0 if fft is not on */
+	unsigned int log_len; /**< log2(length) */
+	int cur_max;          /**< The max data[i] for the current sample set */
+	int hist_max;         /**< The max data[i] since last init_fft() */
+	int max;              /**< The theoretical max, based on sample types */
 };
-extern struct fft_data fft;
+
+extern const struct fft_data *fft;
 void init_fft(void);
 void free_fft(void);
+void QuitFFT(void);
