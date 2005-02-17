027d2b867f7095caa44756296dd6999500c099f8
marf <marf>
2005-02-17 04:52:59 +0000

    Contains some necessary info to run marfitude

---
+++ marfitude/marfitude.h
@@ -0,0 +1,16 @@
+
+#define BLOCK_HEIGHT .75
+#define TIC_HEIGHT .25
+#define BLOCK_WIDTH 2.0
+#define NOTE_WIDTH .75
+
+#define NEGATIVE_TICKS 7*6
+#define POSITIVE_TICKS 57*6
+#define NUM_TICKS 64*6
+#define TIC_ERROR 5 /* number of tics we can be off when hitting a button */
+#define LINES_PER_AP 8
+#define ROWS_PER_LINE 4
+
+#define UNMUTE 0
+#define MUTE 1
+
