cc56a796d02026fd96d609a0cd87a754b7c8e337
marf <marf>
2005-02-26 21:02:28 +0000

    Put the onus of calling init/exit functions onto the dynamic loader

--- util/plugin.c
+++ util/plugin.c
@@ -18,6 +18,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "plugin.h"
 #include "dl.h"
@@ -26,58 +27,35 @@
  * Implements loading and freeing plugins
  */
 
-/** This structure contains the init/exit functions of a plugin. Also contains
- * the reference to the underlying dynamic library.
- */
-struct plugin {
-	int (*init)(void);  /**< Initializes the plugin. Returns 0 on success */
-	void (*exit)(void); /**< The function called on unload */
-	void *handle;       /**< Dynamic library handle */
-};
-
 /** Returns a plugin handle for the plugin @a file */
 void *load_plugin(const char *file)
 {
 	void *handle;
-	struct plugin *p = NULL;
+	char *error;
+	char *path;
 
-	dlerror();
+	path = malloc(strlen(file) + strlen(MARFSHLIBEXT) + 3);
+	strcpy(path, "./");
+	strcat(path, file);
+	strcat(path, MARFSHLIBEXT);
 
-	handle = dlopen(file, RTLD_NOW);
-	if(handle != NULL) {
-		int (*initf)(void);
-		void (*exitf)(void);
+	error = dlerror();
 
-		initf = (int(*)(void))dlsym(handle, "init_plugin");
-		exitf = (void(*)(void))dlsym(handle, "exit_plugin");
-		if(initf != NULL && exitf != NULL) {
-			int ret;
-			p = malloc(sizeof(struct plugin));
-			p->handle = handle;
-			p->init = initf;
-			p->exit = exitf;
-
-			ret = p->init();
-			if(ret != 0) {
-				printf("Error: Plugin returned %i on initialize.\n", ret);
-			}
-		} else {
-			printf("Error: Couldn't load init_plugin and exit_plugin symbols from %s\n", file);
-		}
-	} else {
-		printf("%s\n", dlerror());
-		printf("Error: Couldn't load plugin: %s\n", file);
+	handle = dlopen(path, RTLD_NOW);
+	if(handle == NULL) {
+		error = dlerror();
+		if(error != NULL)
+			printf("%s\n", error);
+		printf("Error: Couldn't load plugin: %s\n", path);
 	}
-	return p;
+	free(path);
+	return handle;
 }
 
 /** Frees the plugin handle that was returned by load_plugin */
-void free_plugin(void *plugin)
+void free_plugin(void *handle)
 {
-	struct plugin *p = plugin;
-	if(p != NULL) {
-		p->exit();
-		dlclose(p->handle);
-		free(p);
+	if(handle != NULL) {
+		dlclose(handle);
 	}
 }
--- util/plugin.h
+++ util/plugin.h
@@ -19,21 +19,9 @@
 /** @file
   * Support for plugins.
   *
-  * Plugins here are defined as shared libraries that export two symbols:
-  * @a init_plugin - defined as int init_plugin(void)
-  * @a exit_plugin - defined as void exit_plugin(void)
-  *
-  * When a plugin is loaded, the init_plugin function is called. When the
-  * plugin is freed, exit_plugin is called.
+  * This really just wraps the dlopen() / dlclose() calls for no particular
+  * reason other than to make the error messages consistent.
   */
-
-/** Macro to define the @a init_plugin function */
-#define plugin_init(initfn) \
-	int init_plugin(void) __attribute__((alias(#initfn)))
-
-/** Macro to define the @a exit_plugin function */
-#define plugin_exit(exitfn) \
-	void exit_plugin(void) __attribute__((alias(#exitfn)))
 
 void *load_plugin(const char *file);
 void free_plugin(void *p);
