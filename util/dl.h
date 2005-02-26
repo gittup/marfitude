fe6c567d3938a90d2a1a8f9192008a40e16716f8
marf <marf>
2005-02-26 21:01:59 +0000

    Added windows defines

--- util/dl.h
+++ util/dl.h
@@ -1,27 +1,14 @@
-/*
-   Copyright (C) 2005 Mike Shal
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
-
-/** @file
- * Wraps libdl for arches that don't have it
- */
 #if defined(__linux__) || defined(__FreeBSD__)
-#include <dlfcn.h>
+# include <dlfcn.h>
 #else
-#error "This platform does not have dynamic library support!"
+# if defined(WIN32)
+#  include <windows.h>
+#  define dlopen(l, a) LoadLibrary(l)
+#  define dlclose FreeLibrary
+#  define dlsym GetProcAddress
+#  define dlerror() NULL
+# else
+#  error "This platform does not have dynamic library support!"
+# endif
 #endif
 /* WIN32 or _WIN32 ? MacOSX */
