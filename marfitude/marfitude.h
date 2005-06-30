9a7afe75496d49b48cf82b877f73382c71cf6745
marf <marf>
2005-06-30 02:08:12 +0000

    Added some more marfitude getters

--- marfitude/marfitude.h
+++ marfitude/marfitude.h
@@ -13,11 +13,23 @@
 #define UNMUTE 0
 #define MUTE 1
 
+/** A structure of score information */
 struct marfitude_score {
 	int highscore;    /**< The previous high score */
 	int score;        /**< The player's current score */
 	int multiplier;   /**< The player's current multiplier */
 };
 
-struct wam *marfitude_get_wam(void);
-void marfitude_get_score(struct marfitude_score *);
+/** A structure of time/position information */
+struct marfitude_pos {
+	double modtime;        /**< Time in seconds */
+	double tic;            /**< Current tic position */
+	const struct row *row; /**< Current row */
+	int row_index;         /**< Row index */
+	int channel;           /**< Which channel is currently played */
+};
+
+const struct wam *marfitude_get_wam(void);
+const int *marfitude_get_offsets(void);
+const struct marfitude_score *marfitude_get_score(void);
+void marfitude_get_pos(struct marfitude_pos *);
