72620a4c75e6d5319a8dde5ca629ee75a4244c2b
marf <marf>
2005-08-23 20:13:23 +0000

    No need for this crap. Wams are magic now.

--- gmae/wam/Makefile.mk
+++ gmae/wam/Makefile.mk
@@ -2,10 +2,5 @@ INSTALL_DIR = $(DATADIR)/wam
 INSTALL_LIST = README
 EXEC = text
 
-wamdir = $(subst //,/,$(DESTDIR)/$(INSTALLDIR)/wam)
-wamfiles = $(wildcard $(wamdir)/*.wam)
+wamfiles = $(wildcard $(DATADIR)/wam/*.wam)
 UNINSTALL_FILES += $(wamfiles)
-
-install: $(wamfiles)
-$(wamdir)/%.wam: gmae/wam.c
-	@$(call RM,$@)
