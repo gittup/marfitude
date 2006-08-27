e01b2aeb6178fd6169349298c1f796554276a6b9
marf <marf>
2006-08-27 16:29:04 +0000

    Update copyright

--- gmae/sdlfatalerror.c
+++ gmae/sdlfatalerror.c
@@ -1,5 +1,5 @@
 /*
-   Copyright (C) 2004 Mike Shal
+   Copyright (C) 2006 Mike Shal
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
